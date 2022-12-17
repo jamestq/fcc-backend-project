f9666dffb6e45b8de55d57cb5fee0217795a235c
James UQ <57053746+jamestq@users.noreply.github.com>
2022-12-17 18:15:01 +1100

    amend date api endpoint to returnt the correct json response

--- index.js
+++ index.js
@@ -2,39 +2,36 @@
 //set up required variables
 let express = require("express");
 let app = express();
-const { convertToUnix, formatDate} = require("./utility/helper");
+const { convertToUnix, formatDate } = require("./utility/helper");
 
 //enable CORS so that the API can be tested by FCC
 let cors = require("cors");
-app.use(cors({optionsSuccessStatus: 200})); //some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 })); //some legacy browsers choke on 204
 
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/index.html");
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 //API endpoints
-app.get("/api/:date?", 
-    (req, res, next) => {
-        let date = formatDate(req.params.date); 
-        if(date === null){
-            res.json({
-                error: "Invalid Date"
-            })
-        }
-        req.date = date;
-        next();
-    }, 
-    (req, res) => {
-        res.json({
-            unix: convertToUnix(req.date),
-            utc:  req.date.toUTCString()
-        })
-    })
+app.get("/api/:date?",
+  (req, res) => {
+    let date = formatDate(req.params.date);
+    if (date === null) {
+      res.json({
+        error: "Invalid Date"
+      })
+    } else {
+      res.json({
+        unix: convertToUnix(date),
+        utc: date.toUTCString()
+      })
+    }
+  })
 
 //listen for requests
 let listener = app.listen(process.env.PORT, () => {
-    console.log("Your app is listening on port " + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 })
 
