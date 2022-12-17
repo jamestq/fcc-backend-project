160d19b783b77c4509856609b283463b850e2403
James UQ <57053746+jamestq@users.noreply.github.com>
2022-12-17 18:13:45 +1100

    modify formatDate function to check valid date

--- utility/helper.js
+++ utility/helper.js
@@ -1,23 +1,19 @@
 const convertToUnix = (date) => {
     return date.getTime();
-}
-
-const formatDate = (dateString) => {
-    if(dateString === undefined){
-        let date = new Date();
-        return date;
-    }else if (!dateString.includes("-")){
-        dateString = parseInt(dateString);
+  }
+  
+  const formatDate = (dateString) => {
+    if (dateString === undefined) {
+      let date = new Date();
+      return date;
+    } else if (dateString.match(/^[0-9]+$/) != null) {
+      dateString = parseInt(dateString);
     }
-    try{
-        let date = new Date(dateString);
-        return date;
-    }catch(invalidDate){
-        return null;
-    }
-}
-
-module.exports = {
-    convertToUnix : convertToUnix,
+    let date = new Date(dateString);
+    return isNaN(Date.parse(date)) ? null : date;
+  }
+  
+  module.exports = {
+    convertToUnix: convertToUnix,
     formatDate: formatDate
-}
+  }
