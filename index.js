d5a24698ff8d8b10a942faf854199ab8149c61e2
jamestq <57053746+jamestq@users.noreply.github.com>
2023-02-02 09:33:34 +1100

    ammend relative path to public

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors({ optionsSuccessStatus: 200 })); //some legacy browsers choke on 20
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 //import routers
 const apiRoute = require("./routes/api");
